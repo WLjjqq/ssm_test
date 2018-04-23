4d5b2b50728c50466ba5080b0f0ada32cc9ecca0
luwei <1422935801@qq.com>
2018-04-23 19:54:13 +0800

    知识点1：在mybatis中写复杂的Sql查询语句

--- src/main/java/com/ssm/mapper/AScMapper.java
+++ src/main/java/com/ssm/mapper/AScMapper.java
@@ -1,9 +1,52 @@
 package com.ssm.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public interface AScMapper {
+    /**
+     * 查询学生平均成绩及其名次
+     * @return
+     */
     List<Map<String,Object>> selectAvg();
+
+    /**
+     * 查询各科成绩最高和最低的分：以如下形式显示：课程ID，最高分，最低分
+     * @return
+     */
     List<Map<String,Object>> selectMaxAndMin();
+    /**
+     *  查询“001”课程比“002”课程成绩高的所有学生的学号；
+     * select a.s_id,a.score
+     * FROM
+     * (select aa.s_id,aa.score
+     * from a_sc aa
+     * where aa.c_id ='001') a,
+     * (select bb.s_id,bb.score
+     * from a_sc bb
+     * where bb.c_id ='002') b
+     * where a.score>b.score
+     * and a.s_id=b.s_id
+     */
+    List<Map<String,Object>> selectAGreaterB(HashMap map);
+
+    /**
+     * 查询平均成绩大于60分的同学的学号和平均成绩；
+     */
+    List<Map<String,Object>> selectAvgGreaterScore(@Param("score") Integer score);
+
+    /**
+     * 查询所有同学的学号、姓名、选课数、总成绩；
+     * @return
+     */
+    List<Map<String,Object>> selectCountXuanKeShu();
+
+    /**
+     * 查询李姓老师有多少个
+     * @return
+     */
+    int selectCountLiTeacher();
 }
--- src/main/test/A_ScTest.java
+++ src/main/test/A_ScTest.java
@@ -3,6 +3,7 @@ import com.ssm.service.AScService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,5 +27,55 @@ public class A_ScTest extends BaseTest {
              ) {
             System.out.println(m);
         }
+    }
+
+    /**
+     * 测试 查询“001”课程比“002”课程成绩高的所有学生的学号；
+     * 因为是传递两个参数。所以用map封装起来。cId对应的就是第一个参数。cId2对应的是第二个参数。
+     */
+    @Test
+    public void testSelectAGreaterB(){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("cId",001);
+        map.put("cId2",002);
+        List<Map<String,Object>> list = ascMapper.selectAGreaterB(map);
+        for (Map<String,Object> i:list
+             ) {
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * 查询平均成绩大于60分的同学的学号和平均成绩；
+     */
+    @Test
+    public void testSelectAvgGreaterScore(){
+        Integer score=60;
+        List<Map<String, Object>> list = ascMapper.selectAvgGreaterScore(score);
+        for (Map<String,Object> i:list
+                ) {
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * 查询所有同学的学号、姓名、选课数、总成绩；
+     */
+    @Test
+    public void testSelectCountXuanKeShu(){
+        List<Map<String, Object>> list = ascMapper.selectCountXuanKeShu();
+        for (Map<String,Object> i:list
+                ) {
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * 查询所有李姓老师
+     */
+    @Test
+    public void testSelectCountLiTeacher(){
+        int a = ascMapper.selectCountLiTeacher();
+        System.out.println(a);
     }
 }
